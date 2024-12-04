7593b42ae9be59d2824058c9b0a1b05919645611
Alberto Peralta <albertoperalta77@hotmail.com>
2024-12-04 14:16:12 -0600

    probando desplazamiento de cards

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const carousels = ["services-carousel", "social-carousel"].map((id) => document.getElementById(id));
     const slideCarousel = (carousel) => {
         if (!carousel) return;
-        const scrollAmount = 250; // Cantidad de desplazamiento
+        const scrollAmount = 350; // Cantidad de desplazamiento
         const maxScrollLeft = carousel.scrollWidth - carousel.clientWidth; // Máximo desplazamiento
         // Desplazarse normalmente
         carousel.scrollBy({ left: scrollAmount, behavior: "smooth" });
